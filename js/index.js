5e5212be4329324bdcf4e0fe80c575d85a089b00
mahery2-codeperso <maheryrakotomalala917@gmail.com>
2026-09-22 01:34:02 +0200

    Update Portfolio

--- js/index.js
+++ js/index.js
@@ -6,16 +6,19 @@ button.addEventListener("click",() => {
     document.body.style.backgroundColor = y%2 === 1 ? "black" : "white";
     document.body.style.color = y%2 === 0 ? "black" : "white";
 
+    // si le curseur encore dessus alors adapter les couleurs
     if(button.matches(":hover"))
     {
         button.style.boxShadow = y%2 === 1 ? "0 0 10px #424242" : "0 0 10px #b6b5b5";
     }
 })
 
+// si le curseur est dessus
 button.addEventListener("mouseover", () =>{
     button.style.boxShadow = y%2 === 1 ? "0 0 10px #424242" : "0 0 10px #b6b5b5";
 })
 
+// si le curseur n'est plus dessus
 button.addEventListener("mouseout", () =>{
     button.style.boxShadow = "none";
 })
