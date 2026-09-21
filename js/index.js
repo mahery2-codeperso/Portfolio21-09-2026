712bbe217d9f2793a831dc5932ddab7f129ee95b
mahery2-codeperso <maheryrakotomalala917@gmail.com>
2026-09-22 01:56:39 +0200

    Update Portfolio

--- js/index.js
+++ js/index.js
@@ -1,24 +1,23 @@
 const button = document.querySelector(".theme");
 let y = 0
-button.addEventListener("click",() => {
+
+// pour pc et telephone ("click" pour pc, "touchstart" pour mobile, "pointerdown" pour les deux (recommandé))
+button.addEventListener("pointerdown", () => {
     y++
     button.style.backgroundColor = y%2 === 1 ? "black" : "white";
     document.body.style.backgroundColor = y%2 === 1 ? "black" : "white";
     document.body.style.color = y%2 === 0 ? "black" : "white";
-
-    // si le curseur encore dessus alors adapter les couleurs
-    if(button.matches(":hover"))
-    {
-        button.style.boxShadow = y%2 === 1 ? "0 0 10px #424242" : "0 0 10px #b6b5b5";
-    }
-})
-
-// si le curseur est dessus
-button.addEventListener("mouseover", () =>{
     button.style.boxShadow = y%2 === 1 ? "0 0 10px #424242" : "0 0 10px #b6b5b5";
 })
 
-// si le curseur n'est plus dessus
-button.addEventListener("mouseout", () =>{
+// si le curseur ou doigt est dessus
+button.addEventListener("pointerenter", () =>{
+    button.style.boxShadow = y%2 === 1 ? "0 0 10px #424242" : "0 0 10px #b6b5b5";
+})
+
+// si le curseur ou doigt n'est plus dessus
+button.addEventListener("pointerleave", () =>{
     button.style.boxShadow = "none";
 })
+
+// pointerdown = click; pointerenter = mouseover; pointerleave = mouseout;
