38170aa7e1e1518a768f7c8bb8968b5221691a3d
mahery2-codeperso <maheryrakotomalala917@gmail.com>
2026-09-22 02:02:34 +0200

    Update Portfolio

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 const button = document.querySelector(".theme");
-let y = 0
+let y = 0;
 
 // pour pc et telephone ("click" pour pc, "touchstart" pour mobile, "pointerdown" pour les deux (recommandé))
 button.addEventListener("pointerdown", () => {
-    y++
+    y++;
     button.style.backgroundColor = y%2 === 1 ? "black" : "white";
     document.body.style.backgroundColor = y%2 === 1 ? "black" : "white";
     document.body.style.color = y%2 === 0 ? "black" : "white";
     button.style.boxShadow = y%2 === 1 ? "0 0 10px #424242" : "0 0 10px #b6b5b5";
-})
+});
 
 // si le curseur ou doigt est dessus
 button.addEventListener("pointerenter", () =>{
     button.style.boxShadow = y%2 === 1 ? "0 0 10px #424242" : "0 0 10px #b6b5b5";
-})
+});
 
 // si le curseur ou doigt n'est plus dessus
 button.addEventListener("pointerleave", () =>{
     button.style.boxShadow = "none";
-})
+});
 
 // pointerdown = click; pointerenter = mouseover; pointerleave = mouseout;
